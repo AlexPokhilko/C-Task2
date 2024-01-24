01b8b63b15fbbc22472bc75b95956c1932f071a5
alex.pokhilko <alex.pokhilko@gmail.com>
2024-01-25 00:29:49 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int a1 =100;
+int a2 =200;
+int sum;
+
+sum = a1 +a2;
+
+Console.WriteLine(sum);
